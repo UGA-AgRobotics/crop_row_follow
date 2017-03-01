3d049a2186a88c7d41c271f5e56d6a70c041883f
Brad Bazemore <bwbazemore@uga.edu>
2017-03-01 09:54:56 -0500

    added some canny edge detection

--- scripts/crop_row_follow.py
+++ scripts/crop_row_follow.py
@@ -9,34 +9,36 @@ u_blur = cv2.GaussianBlur(u, (41, 41), 0)
 
 v_blur = cv2.GaussianBlur(v, (41, 41), 0)
 
-rtu, u_thresh = cv2.threshold(u_blur, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+rtu, u_thresh = cv2.threshold(u_blur, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
 
-rtv ,v_thresh = cv2.threshold(v_blur, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+rtv, v_thresh = cv2.threshold(v_blur, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
 
 img_and = cv2.bitwise_and(u_thresh, v_thresh)
 
-plt.subplot(231)
-plt.title("U Gauss Blur")
-plt.imshow(u_blur, cmap='gray')
+edges = cv2.Canny(img_and, 10, 50, 2, L2gradient=True)
 
-plt.subplot(232)
-plt.title("V Gauss Blur")
-plt.imshow(v_blur, cmap='gray')
+# plt.subplot(231)
+# plt.title("U Gauss Blur")
+# plt.imshow(u_blur, cmap='gray')
+#
+# plt.subplot(232)
+# plt.title("V Gauss Blur")
+# plt.imshow(v_blur, cmap='gray')
+#
+# plt.subplot(233)
+# plt.title("U Otsu Threshold")
+# plt.imshow(u_thresh, cmap='gray')
+#
+# plt.subplot(234)
+# plt.title("V Otsu Threshold")
+# plt.imshow(v_thresh, cmap='gray')
 
-plt.subplot(233)
-plt.title("U Hist")
-plt.hist(u.ravel(), 256, [0, 256])
-
-plt.subplot(234)
-plt.title("U Otsu Threshold")
-plt.imshow(u_thresh)
-
-plt.subplot(235)
-plt.title("V Otsu Threshold")
-plt.imshow(v_thresh)
-
-plt.subplot(236)
-plt.title("U & V")
-plt.imshow(img_and)
+# plt.subplot(121)
+# plt.title("U & V")
+# plt.imshow(img_and, cmap='gray')
+#
+# plt.subplot(122)
+plt.title("Edges")
+plt.imshow(edges, cmap='gray')
 
 plt.show()
