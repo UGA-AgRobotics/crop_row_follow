84b77f1d64fb5195a8ec112743641543c556b338
Brad Bazemore <bwbazemore@uga.edu>
2017-03-21 22:35:33 -0400

    simple params

--- scripts/dev_idk.py
+++ scripts/dev_idk.py
@@ -2,6 +2,11 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
+OK = 10
+CK = 20
+hough_thresh = 700
+hough_min_line = 600
+hough_max_gap = 50
 
 image = cv2.imread('../img/test1.png', cv2.IMREAD_COLOR)
 # blur the image to get rid of some of that noise
@@ -11,8 +16,8 @@ b, g, r = cv2.split(blur)
 # increase the amount of green relative to red and blue
 I = 2 * g - r - b
 # define the kernels for opening and closing
-open_k = np.ones((40, 40), np.uint8)
-close_k = np.ones((60, 60), np.uint8)
+open_k = np.ones((OK, OK), np.uint8)
+close_k = np.ones((CK, CK), np.uint8)
 # close the image to get rid of the small noise
 closed = cv2.morphologyEx(I, cv2.MORPH_CLOSE, close_k)
 # open to fill in any gaps
@@ -20,18 +25,18 @@ opened = cv2.morphologyEx(closed, cv2.MORPH_OPEN, open_k)
 # threshold to binary with Otsu
 rt, th = cv2.threshold(opened, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
 # invert because Hough looks for 255
-th = 255-th
+th = 255 - th
 # deep copy the image for Hough
 hough_image = np.matrix.copy(image)
 # find the Hough lines using the PPHT method
-lines = cv2.HoughLinesP(image=th, rho=1, theta=np.pi / 180, threshold=2500, minLineLength=600, maxLineGap=200)
+lines = cv2.HoughLinesP(image=th, rho=1, theta=np.pi / 180, threshold=hough_thresh, minLineLength=hough_min_line,
+                        maxLineGap=hough_max_gap)
 if lines is not None:
     for x in range(0, len(lines)):
         for x1, y1, x2, y2 in lines[x]:
             cv2.line(hough_image, (x1, y1), (x2, y2), (255, 0, 0), 10)
 else:
     print "No lines"
-
 
 plt.subplot(231)
 plt.title("Image")
