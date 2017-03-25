ee2524c7b84dc61f15f25bfab4a2e27f3805578e
Brad Bazemore <bwbazemore@uga.edu>
2017-03-25 13:50:36 -0400

    moved new ood code to ros

--- scripts/crop_row_follow.py
+++ scripts/crop_row_follow.py
@@ -8,83 +8,91 @@ from cv_bridge import CvBridge, CvBridgeError
 from sensor_msgs.msg import CompressedImage, Image
 
 
-def crop_line_image(image_raw):
-    OK = 20
-    CK = 20
-    hough_thresh = 220
-    hough_min_line = 100
-    hough_max_gap = 100
-
-    col = 100
-    row = 500
-
-    image = image_raw[col:col + 600, row:row + 400]
-    # blur the image to get rid of some of that noise
-    blur = cv2.GaussianBlur(image, (5, 5), 10)
-    # break image into blue, green, red
-    b, g, r = cv2.split(blur)
-    # increase the amount of green relative to red and blue
-    I = 2 * g - r - b
-    # define the kernels for opening and closing
-    open_k = np.ones((OK, OK), np.uint8)
-    close_k = np.ones((CK, CK), np.uint8)
-    # close the image to get rid of the small noise
-    closed = cv2.morphologyEx(I, cv2.MORPH_CLOSE, close_k)
-    # open to fill in any gaps
-    opened = cv2.morphologyEx(closed, cv2.MORPH_OPEN, open_k)
-    # threshold to binary with Otsu
-    rt, th = cv2.threshold(opened, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-    # invert because Hough looks for 255
-    th = 255 - th
-    # deep copy the image for Hough
-    hough_image = image
-    # find the Hough lines using the PPHT method
-    lines = cv2.HoughLinesP(image=th, rho=1, theta=np.pi / 100, threshold=hough_thresh, minLineLength=hough_min_line,
-                            maxLineGap=hough_max_gap)
-    if lines is not None:
-        for x in range(0, len(lines)):
-            count = 0
-            for x1, y1, x2, y2 in lines[x]:
-                angle = math.atan2(y1-y2, x1-x2) * (180 / math.pi)
-                if 135 > angle > 45:
-                    cv2.line(hough_image, (x1, y1), (x2, y2), (0, 0, 255), 2)
-                    count += 1
-                else:
-                    pass
-        rospy.loginfo('Lines %d', len(lines))
-    else:
-        rospy.logwarn('No lines detected')
-
-    return hough_image
-
-
-class ImgProc(object):
+class CropRowFind(object):
     def __init__(self):
-        self.img_pub = rospy.Publisher('crop_rows', Image, queue_size=1)
+        self.vision = VisionCV2()
+        self.rows = None
+
+    def find_rows(self, data):
+        self.rows = self.vision.find_rows(data)
+        return self.rows
+
+    def draw_rows(self, img):
+        if self.rows is not None:
+            for x in range(0, len(self.rows)):
+                for x1, y1, x2, y2 in self.rows[x]:
+                    cv2.line(img, (x1, y1), (x2, y2), (255, 0, 0), 10)
+        return img
+
+
+class VisionCV2(CropRowFind):
+    def __init__(self):
+        self.window = (150, 450, 580, 880)
+        self.sigma = 10
+        self.gauss_kernel = (5, 5)
+        self.close_open_kernels = ((20, 20), (10, 10))
+        self.hough_params = (1, 180, 250, 100, 50)
+
+    def find_rows(self, data):
+        # img = self.roi(data)
+        img = self.blur(data)
+        img = self.egvi(img)
+        img = self.close_open(img)
+        rt, img = self.threshold(img)
+        return self.lines(img)
+
+    def roi(self, img):
+        return img[self.window[0]:self.window[1], self.window[2]:self.window[3]]
+
+    def blur(self, img):
+        return cv2.GaussianBlur(img, self.gauss_kernel, self.sigma)
+
+    def egvi(self, img):
+        b, g, r = cv2.split(img)
+        return 2 * g - r - b
+
+    def close_open(self, img):
+        closed = cv2.morphologyEx(img, cv2.MORPH_CLOSE, np.ones(self.close_open_kernels[0], np.uint8))
+        return cv2.morphologyEx(closed, cv2.MORPH_OPEN, np.ones(self.close_open_kernels[1], np.uint8))
+
+    def threshold(self, img):
+        return cv2.threshold(img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+
+    def lines(self, img):
+        return cv2.HoughLinesP(image=img, rho=self.hough_params[0], theta=np.pi / self.hough_params[1],
+                               threshold=self.hough_params[2], minLineLength=self.hough_params[3],
+                               maxLineGap=self.hough_params[4])
+
+
+class CropRowFollow(object):
+    def __init__(self):
+        self.rows_img_pub = rospy.Publisher('rows_img', Image, queue_size=1)
         self.img_sub = rospy.Subscriber('camera/image_color/compressed', CompressedImage, self.crop_image_cb)
         self.bridge = CvBridge()
+        self.crf = CropRowFind()
 
     def crop_image_cb(self, data):
         try:
             img = self.bridge.compressed_imgmsg_to_cv2(data)
-            lines_img = crop_line_image(img)
-            if lines_img is not None:
-                self.img_pub.publish(self.bridge.cv2_to_imgmsg(lines_img, 'bgr8'))
+            self.crf.find_rows(img)
+            rows_img = self.crf.draw_rows(img)
+            if rows_img is not None:
+                self.rows_img_pub.publish(self.bridge.cv2_to_imgmsg(rows_img, 'bgr8'))
             else:
                 rospy.loginfo('No Image')
         except CvBridgeError as e:
             print e
 
 
-def driver():
+def main():
     rospy.init_node('crop_row_follow', anonymous=True)
-    ImgProc()
+    CropRowFollow()
     while not rospy.is_shutdown():
         rospy.spin()
 
 
 if __name__ == '__main__':
     try:
-        driver()
+        main()
     except rospy.ROSInterruptException:
         pass
